efe11bdebd94073b7c036112a38bd34933e74fcc
ben <bholsinger@gmail.com>
2019-06-04 13:51:22 -0600

    building store and components(starter)

--- src/store.js
+++ src/store.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
+const _api = axios.create({
+  baseURL: ""
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    activeRover: {}
 
   },
   mutations: {
+    setActiveRover(state, data) {
+      state.activeRover = data
+    }
 
   },
   actions: {
